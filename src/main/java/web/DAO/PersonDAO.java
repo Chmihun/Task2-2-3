7683b509ee1f42edd75c3002817ba9429115834b
Sergey <chmihun@gmail.com>
2024-09-22 11:34:40 +0300

    вывел всех пиплов красиво

--- src/main/java/web/DAO/PersonDAO.java
+++ src/main/java/web/DAO/PersonDAO.java
@@ -1,32 +1,32 @@
 package web.DAO;
 
 import org.springframework.stereotype.Component;
-import web.model.Users;
+import web.model.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class UserDAO {
+public class PersonDAO {
     private static int CAR_COUNT;
-    private List<Users> users;
+    private List<Person> users;
 
     {
         users = new ArrayList<>();
 
-        users.add(new Users(++CAR_COUNT, "Name1", "surName1", 2001));
-        users.add(new Users(++CAR_COUNT, "Name2", "surName2", 2002));
-        users.add(new Users(++CAR_COUNT, "Name3", "surName3", 2003));
-        users.add(new Users(++CAR_COUNT, "Name4", "surName4", 2004));
-        users.add(new Users(++CAR_COUNT, "Name5", "surName5", 2005));
+        users.add(new Person(++CAR_COUNT, "Name1", "surName1", 2001));
+        users.add(new Person(++CAR_COUNT, "Name2", "surName2", 2002));
+        users.add(new Person(++CAR_COUNT, "Name3", "surName3", 2003));
+        users.add(new Person(++CAR_COUNT, "Name4", "surName4", 2004));
+        users.add(new Person(++CAR_COUNT, "Name5", "surName5", 2005));
     }
 
-    public List<Users> index() {
+    public List<Person> index() {
         return users;
     }
 
 
-public Users show(int id){
+public Person show(int id){
         return users.stream().filter(users->users.getId()==id).findAny().orElse(null);
 }
 
--- src/main/java/web/controller/PeopleController.java
+++ src/main/java/web/controller/PeopleController.java
@@ -6,38 +6,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 //import web.DAO.CarDAO;
+import web.DAO.PersonDAO;
 import web.Servise.UserServse;
+import web.model.Person;
 
 @Controller
 public class PeopleController {
+    private final PersonDAO personDAO;
 
-
-//    private final CarDAO carDao;
-
-    private final UserServse userServse;
-
-    public PeopleController(UserServse userServse) {
-        this.userServse = userServse;
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
     }
 
-    @GetMapping()
-
-//    public String user(@RequestParam(required = false, defaultValue = "10") int count, Model model) {
-//        model.addAttribute("user", userServse.getUsers(count));
-//        return "user";
-//    }
-
-//    public String user(){
-//        return "user";
-//    }
-    public String index(Model model){
-        model.addAttribute("people",userServse.getUsers(count));
-return null;
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "index";
     }
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id")int id, Model model){
-        return "show";
+
+    @GetMapping("/people/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        Person person = personDAO.show(id);
+        if (person != null) {
+            model.addAttribute("person", person);
+            return "show";
+        } else {
+            model.addAttribute("error", "Человек не найден");
+            return "error"; // взглянуть на страницу ошибки
+        }
     }
 }
